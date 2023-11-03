974fc741b77b94a8fee8204f8997732fdb6973a2
Kirill <kalenik.kirill@gmail.com>
2023-11-03 09:11:29 +0200

    Add homework 5

---
+++ scriptHW5_1.js
@@ -0,0 +1,12 @@
+// 1. Задача на селектори
+let spanWithId = document.querySelector('#headerTwo');
+//console.log(spanWithId);
+
+let spanWithClass = document.querySelector('.firstSection');
+//console.log(spanWithClass);
+
+let spanWithText = document.querySelectorAll('ul > li:not(.text)')[2];
+//console.log(spanWithText);
+
+let spanWithClass2 = document.querySelector('.hatredLevelBlock');
+//console.log(spanWithClass2);
---
+++ scriptHW5_2.js
@@ -0,0 +1,52 @@
+const turnButton = document.querySelector(".redBtn");
+const bodyElem = document.querySelector("body");
+const labelElem = document.querySelector(".lastTurn");
+
+if(localStorage.getItem("Date") === null)
+    localStorage.setItem("Date", setCurDate());
+
+if(localStorage.getItem("buttonState") !== null){
+    if(localStorage.getItem("buttonState") == 'Turn on' ){
+        turnButton.textContent = 'Turn on';
+        darkPage();
+    }
+    else{    
+        turnButton.textContent = 'Turn off'
+        lightPage();
+    }
+}
+
+function changeTurnOffOn(event){
+    if(turnButton.textContent === 'Turn off'){
+        turnButton.textContent = 'Turn on';
+        localStorage.setItem("buttonState", 'Turn on');
+        darkPage();
+        localStorage.setItem('Date', setCurDate());
+    }
+    else{
+        turnButton.textContent = 'Turn off'
+        localStorage.setItem("buttonState", 'Turn off');
+        lightPage();
+        localStorage.setItem('Date', setCurDate());
+    }
+}
+
+function setCurDate(){
+    let curDate = new Date();
+    return curDate.getDate()+'-'+(curDate.getMonth()+1)+'-'+curDate.getFullYear()+' '
+            +curDate.getHours() + ":" + curDate.getMinutes() + ":" + curDate.getSeconds();
+}
+
+function darkPage() {
+    bodyElem.style.backgroundColor = '#404040';
+    labelElem.style.color = 'white';
+    labelElem.textContent = 'Last turn off: ' + localStorage.getItem('Date');
+}
+
+function lightPage() {
+    bodyElem.style.backgroundColor = '#F5F5F5';
+    labelElem.style.color = 'black';
+    labelElem.textContent = 'Last turn on: ' + localStorage.getItem('Date');
+}
+
+turnButton.addEventListener('click', changeTurnOffOn);
